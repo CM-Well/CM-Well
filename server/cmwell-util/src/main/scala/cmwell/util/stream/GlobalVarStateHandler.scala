e44861dccbf400da146eedb1f0e5ccf6cf246ce1
Gilad Hoch <gilad.hoch@thomsonreuters.com>
2017-09-04 14:29:00 +0300

    adding a custom stage to handle global variable state within akka streams

---
+++ server/cmwell-util/src/main/scala/cmwell/util/stream/GlobalVarStateHandler.scala
@@ -0,0 +1,88 @@
+package cmwell.util.stream
+
+import akka.stream.{AmorphousShape, Attributes, Inlet, Outlet}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util._
+
+
+
+/**
+  * Proj: server
+  * User: gilad
+  * Date: 9/4/17
+  * Time: 1:20 PM
+  */
+class GlobalVarStateHandler[State](setters: Int, getters: Int)(initialAsync: () ⇒ Future[State])(implicit ec: ExecutionContext) extends GraphStage[AmorphousShape] {
+  val inlets: List[Inlet[State]] = List.tabulate(setters)(i => Inlet[State]("GlobalVarStateHandler.in" + i))
+  val outlets: List[Outlet[State]] = List.tabulate(getters)(i => Outlet[State]("GlobalVarStateHandler.in" + i))
+
+  override val shape: AmorphousShape = AmorphousShape(inlets,outlets)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+
+    var state: State = null.asInstanceOf[State]
+
+    outlets.foreach { out =>
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          if(state != null) push(out,state)
+          // else when AsyncCallback returns with value,
+          // or when inlets override initial value,
+          // we will replace handlers and return state.
+        }
+      })
+    }
+
+    inlets.foreach { in =>
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          state = grab(in)
+          outlets.foreach { out =>
+            if(isAvailable(out)) push(out,state)
+          }
+          pull(in)
+        }
+      })
+    }
+
+    def replaceHandlers: Unit = {
+      outlets.foreach { out =>
+        setHandler(out, new OutHandler {
+          override def onPull(): Unit = push(out,state)
+        })
+      }
+      inlets.foreach { in =>
+        setHandler(in, new InHandler {
+          override def onPush(): Unit = {
+            state = grab(in)
+            pull(in)
+          }
+        })
+      }
+    }
+
+    override def preStart(): Unit = {
+
+      val acb = getAsyncCallback[Try[State]] {
+        case Failure(ex) if state == null ⇒ failStage(ex)
+        case Failure(ex) if state != null ⇒ replaceHandlers
+        case Success(initialState) ⇒ {
+
+          if(state == null)
+            state = initialState
+
+          outlets.foreach { out =>
+            if(isAvailable(out)) push(out,state)
+          }
+
+          replaceHandlers
+        }
+      }
+
+      initialAsync().onComplete(acb.invoke)
+      inlets.foreach(tryPull)
+    }
+  }
+}
