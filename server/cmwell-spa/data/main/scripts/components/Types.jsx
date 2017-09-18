af69d8f0a1de50f461045e67f2ac63c077669f44
Yaakov Breuer <Yaakov.Breuer@thomsonreuters.com>
2017-09-18 11:12:59 +0300

    WIP

--- server/cmwell-spa/data/main/scripts/components/Types.jsx
+++ server/cmwell-spa/data/main/scripts/components/Types.jsx
@@ -16,6 +16,24 @@ class Type extends React.Component {
     }
 }
 
+class ExpandButton extends React.Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = { }
+    }
+
+    clickCb() {
+        this.props.clickCb && this.props.clickCb()
+//        this.toggleState('expanded')
+    }
+    
+    render() {
+        let className = 'expand-button' + (this.props.isExpanded ? ' expanded' : '')
+        return <img className={className} width="25" height="25" src="/meta/app/main/images/blue-down-arrow.svg" onClick={this.clickCb.bind(this)} />
+    }
+}
+
 class Types extends React.Component {
     constructor(props) {
         super(props)
@@ -28,17 +46,24 @@ class Types extends React.Component {
         this.all = <Type label="All" selected={true} />
     }
         
-    componentWillMount() {
-        console.log('Types will receive props')
+    componentWillReceiveProps() {
         AppUtils.cachedGet(`${this.props.location.pathname}?op=aggregate&recursive&ap=type:term,field::type.rdf,size:1024&format=json`).then(resp => {
             resp.AggregationResponse && this.setState({ types: resp.AggregationResponse[0].buckets.map(bckt => { return { uri: bckt.key, count: bckt.objects } }) })
         })
+        this.setState({ expanded: false })
     }
     
     render() {
         AppUtils.debug('Types.render')
+        let className = `types-container ${this.state.expanded ? 'expanded' : ''}`
         let types = [this.all, ...this.state.types.map(t => <Type uri={t.uri} count={t.count} />)]
-        return <div className="types-container">View by: { AppUtils.addSep(types, this.separator) }</div>
+                                                       
+        let isExpandButtonNeeded = this.state.types.length > 5 // todo is hard-coding 5 is the best we can do here?
+                                                       
+        return <div className={className}>
+            <div className="types">View by: { AppUtils.addSep(types, this.separator) }</div>
+           { isExpandButtonNeeded ? <ExpandButton isExpanded={this.state.expanded} clickCb={this.toggleState.bind(this, 'expanded')}/> : null }
+        </div>
     }
 }
 
