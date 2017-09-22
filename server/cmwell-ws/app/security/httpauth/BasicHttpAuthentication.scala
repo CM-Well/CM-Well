1d0c6690d83dd3f982f6e16abab36ebd2515ad6d
Yoni.Mataraso <yoni.mataraso@tr.com>
2017-09-22 15:49:39 -0400

    Checking for indexOutOfBoundException

--- server/cmwell-ws/app/security/httpauth/BasicHttpAuthentication.scala
+++ server/cmwell-ws/app/security/httpauth/BasicHttpAuthentication.scala
@@ -23,12 +23,15 @@ import scala.util.{Failure, Success, Try}
 trait BasicHttpAuthentication {
   private val authTypePrefix = "Basic "
 
-  def decodeBasicAuth(auth: String): (String, String) = {
+  def decodeBasicAuth(auth: String): Try[(String, String)] = {
     Try(new String(new Base64().decode(auth.drop(authTypePrefix.length)), "UTF-8")) match {
       case Success(userColonPass) =>
         val split = userColonPass.split(":", 2)
-        split(0)->split(1)
-      case Failure(_) => throw new IllegalArgumentException(s"$auth is not a valid Basic header")
+        if (split.length != 2)
+          Failure(new IllegalArgumentException(s"$auth is not a valid Basic header"))
+        else
+          Success(split(0)->split(1))
+      case Failure(_) => Failure(new IllegalArgumentException(s"$auth is not a valid Basic header"))
     }
   }
 }
