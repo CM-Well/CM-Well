9d3907c792616eaa2e93f626eed118cf0e739377
Yaakov Breuer <Yaakov.Breuer@thomsonreuters.com>
2017-09-25 15:30:24 +0300

    changes requested by review

--- server/cmwell-ws/app/security/httpauth/BasicHttpAuthentication.scala
+++ server/cmwell-ws/app/security/httpauth/BasicHttpAuthentication.scala
@@ -16,15 +16,13 @@
 
 package security.httpauth
 
-import org.apache.commons.codec.binary.Base64
-
-import scala.util.{Failure, Success, Try}
+import cmwell.util.string.Base64.decodeBase64String
 
 trait BasicHttpAuthentication {
   private val authTypePrefix = "Basic "
 
   def decodeBasicAuth(auth: String): (String, String) = {
-    val userColonPass = new String(new Base64().decode(auth.drop(authTypePrefix.length)), "UTF-8")
+    val userColonPass = decodeBase64String(auth.drop(authTypePrefix.length), "UTF-8")
     val split = userColonPass.split(":", 2)
     if (split.length != 2)
       throw new IllegalArgumentException(s"`$auth` is not a valid Basic header")
