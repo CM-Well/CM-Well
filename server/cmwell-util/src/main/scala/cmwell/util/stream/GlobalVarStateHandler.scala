6eec958d23ebfab138251a38e72d7a3b175cc6d2
Gilad Hoch <gilad.hoch@thomsonreuters.com>
2017-10-16 17:36:02 +0300

    cherry-pick -n 0622ad8e7a4f2fe5b2d6701df5db8f75788238cf + unstaging ImpStream changes

--- server/cmwell-util/src/main/scala/cmwell/util/stream/GlobalVarStateHandler.scala
+++ server/cmwell-util/src/main/scala/cmwell/util/stream/GlobalVarStateHandler.scala
@@ -15,6 +15,8 @@ import scala.util._
   * Time: 1:20 PM
   */
 class GlobalVarStateHandler[State](setters: Int, getters: Int)(initialAsync: () ⇒ Future[State])(implicit ec: ExecutionContext) extends GraphStage[SingleTypeAmorphousShape[State,State]] {
+  require(setters >= 0 && getters >= 0 && setters + getters > 0, "must have positive number of either inlets or outlets")
+
   val inlets: List[Inlet[State]] = List.tabulate(setters)(i => Inlet[State]("GlobalVarStateHandler.in" + i))
   val outlets: List[Outlet[State]] = List.tabulate(getters)(i => Outlet[State]("GlobalVarStateHandler.in" + i))
 
@@ -23,9 +25,14 @@ class GlobalVarStateHandler[State](setters: Int, getters: Int)(initialAsync: () 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
 
     var state: State = null.asInstanceOf[State]
+    var hasActiveSetters: Boolean = setters > 0
 
     outlets.foreach { out =>
       setHandler(out, new OutHandler {
+        override def onDownstreamFinish(): Unit = {
+          if(inlets.forall(isClosed[State]) && outlets.forall(isClosed[State]))
+            completeStage()
+        }
         override def onPull(): Unit = {
           if(state != null) push(out,state)
           // else when AsyncCallback returns with value,
@@ -37,6 +44,13 @@ class GlobalVarStateHandler[State](setters: Int, getters: Int)(initialAsync: () 
 
     inlets.foreach { in =>
       setHandler(in, new InHandler {
+        override def onUpstreamFinish(): Unit = {
+          if(inlets.forall(isClosed[State])) {
+            if (outlets.forall(isClosed[State])) {
+              completeStage()
+            } else hasActiveSetters = false
+          }
+        }
         override def onPush(): Unit = {
           state = grab(in)
           outlets.foreach { out =>
@@ -50,11 +64,19 @@ class GlobalVarStateHandler[State](setters: Int, getters: Int)(initialAsync: () 
     def replaceHandlers: Unit = {
       outlets.foreach { out =>
         setHandler(out, new OutHandler {
+          override def onDownstreamFinish(): Unit = {
+            if(inlets.forall(isClosed[State]) && outlets.forall(isClosed[State]))
+              completeStage()
+          }
           override def onPull(): Unit = push(out,state)
         })
       }
       inlets.foreach { in =>
         setHandler(in, new InHandler {
+          override def onUpstreamFinish(): Unit = {
+            if(inlets.forall(isClosed[State]) && outlets.forall(isClosed[State]))
+              completeStage()
+          }
           override def onPush(): Unit = {
             state = grab(in)
             pull(in)
@@ -66,8 +88,8 @@ class GlobalVarStateHandler[State](setters: Int, getters: Int)(initialAsync: () 
     override def preStart(): Unit = {
 
       val acb = getAsyncCallback[Try[State]] {
-        case Failure(ex) if state == null ⇒ failStage(ex)
-        case Failure(ex) if state != null ⇒ replaceHandlers
+        case Failure(ex) if state != null && hasActiveSetters ⇒ replaceHandlers
+        case Failure(ex) ⇒ failStage(ex)
         case Success(initialState) ⇒ {
 
           if(state == null)
