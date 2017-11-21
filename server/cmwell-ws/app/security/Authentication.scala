85f4060d9c79ea638cd8ff1dcf1dd5474c09459a
Yaakov Breuer <Yaakov.Breuer@thomsonreuters.com>
2017-11-21 04:34:49 +0200

    remove commented out code

--- server/cmwell-ws/app/security/Authentication.scala
+++ server/cmwell-ws/app/security/Authentication.scala
@@ -16,11 +16,7 @@
 
 package security
 
-import authentikat.jwt._
-import cmwell.ws.Settings
 import com.github.t3hnar.bcrypt._
-import com.typesafe.config.ConfigFactory
-import org.joda.time.DateTime
 import play.api.libs.json.JsValue
 
 /**
@@ -28,46 +24,7 @@ import play.api.libs.json.JsValue
  */
 
 object Authentication {
-
-//  private lazy val secret = ConfigFactory.load().getString("play.http.secret.key") // not using ws.Settings, so it'd be available from `sbt ws/console`
-//  private val jwtHeader = JwtHeader("HS256")
-
   def passwordMatches(user: JsValue, password:String): Boolean = {
     password.isBcrypted((user\"digest").asOpt[String].getOrElse(""))
   }
-
-//  def generateToken(username: String, expiry: Option[DateTime] = None, rev: Option[Int] = None, isAdmin: Boolean = false): String = {
-//    val maxDays = Settings.maxDaysToAllowGenerateTokenFor
-//    if(!isAdmin && expiry.isDefined && expiry.get.isAfter(DateTime.now.plusDays(maxDays)))
-//      throw new IllegalArgumentException(s"Token expiry must be less than $maxDays days")
-//    if(!isAdmin && rev.isDefined)
-//      throw new IllegalArgumentException(s"rev should only be supplied in Admin mode (i.e. manually via console)")
-//
-//    val claims = Map("sub" -> username, "exp" -> expiry.getOrElse(DateTime.now.plusDays(1)).getMillis, "rev" -> rev.getOrElse(getUserRevNum(username)))
-//    JsonWebToken(jwtHeader, JwtClaimsSet(claims), secret)
-//  }
-
-//  def validateToken(jwt: String) = {
-//    jwt match {
-//      case JsonWebToken(_, claimsSet, _) =>
-//        JsonWebToken.validate(jwt, secret) &&
-//          new DateTime(claimsSet.getClaimValue("exp").map(_.toLong).getOrElse(0L)).isAfterNow &&
-//          claimsSet.getClaimValue("rev").map(_.toInt).getOrElse(0) >= claimsSet.getClaimValue("sub").map(getUserRevNum).getOrElse(0)
-//      case _ => false
-//    }
-//  }
-//
-//  def extractUsernameFromToken(jwt: String): Option[String] = {
-//    jwt match {
-//      case JsonWebToken(_, claimsSet, _) => claimsSet.getClaimValue("sub")
-//      case _ => None
-//    }
-//  }
-
-//  private def getUserRevNum(username: String) = AuthCache.getUserInfoton(username).flatMap(u => (u\"rev").asOpt[Int]).getOrElse(0)
-
-
-//  implicit class JwtClaimsSetJValueExtensions(cs: JwtClaimsSetJValue) {
-//    def getClaimValue(key: String): Option[String] = cs.asSimpleMap.toOption.getOrElse(Map()).get(key)
-//  }
 }
