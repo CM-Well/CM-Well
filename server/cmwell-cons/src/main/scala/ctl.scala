225a725340916b090d56c931fa9d1be1f91b5585
Moria Yechiel <moria.yechiel@thomsonreuters.com>
2018-06-10 19:00:48 +0300

    Fix java_home to be calculated on remote side

--- server/cmwell-cons/src/main/scala/ctl.scala
+++ server/cmwell-cons/src/main/scala/ctl.scala
@@ -1615,9 +1615,10 @@ abstract class Host(user: String,
     val replicationFactor = math.min(hosts.size, 3)
 
     val javaHomeLocation = s"${instDirs.globalLocation}/cm-well/app/java"
-    val exportCommand = if (Files.exists(Paths.get(javaHomeLocation)))
-      s"export PATH=${instDirs.globalLocation}/cm-well/app/java/bin:$$PATH ; export JAVA_HOME=${javaHomeLocation} ;"
-      else ""
+    val exportCommand = s"""if [ -d $javaHomeLocation ]
+        then export PATH=$javaHomeLocation/cm-well/app/java/bin:$$PATH ;
+        export JAVA_HOME=$javaHomeLocation ;
+        fi ; """
 
     // scalastyle:off
     val createTopicCommandPrefix = s"cd ${instDirs.globalLocation}/cm-well/app/kafka/cur; $exportCommand sh bin/kafka-topics.sh --create --zookeeper ${pingAddress}:2181 --replication-factor $replicationFactor --partitions ${hosts.size} --topic"
@@ -1874,9 +1875,12 @@ abstract class Host(user: String,
     val replicationFactor = math.min(ips.size, 3)
 
     val javaHomeLocation = s"${absPath(instDirs.globalLocation)}/cm-well/app/java"
-    val exportCommand = if (Files.exists(Paths.get(javaHomeLocation)))
-      s"export PATH=${absPath(instDirs.globalLocation)}/cm-well/app/java/bin:$$PATH ; export JAVA_HOME=${javaHomeLocation} ;"
-    else ""
+
+    val exportCommand = s"""if [ -d $javaHomeLocation ]
+        then export PATH=$javaHomeLocation/cm-well/app/java/bin:$$PATH ;
+        export JAVA_HOME=$javaHomeLocation ;
+        fi ; """
+
     // scalastyle:off
     val createTopicCommandPrefix = s"cd ${absPath(instDirs.globalLocation)}/cm-well/app/kafka/cur; $exportCommand sh bin/kafka-topics.sh --create --zookeeper ${pingAddress}:2181 --replication-factor $replicationFactor --partitions ${ips.size} --topic"
 
