6341d881dccaf13c3655ecfba1bb1e0781fe9b89
Moria Yechiel <moria.yechiel@thomsonreuters.com>
2018-06-14 13:06:16 +0300

    Remove unneeded ==true

--- server/cmwell-it/src/it/scala/cmwell/it/PluginsFunctionalityTests.scala
+++ server/cmwell-it/src/it/scala/cmwell/it/PluginsFunctionalityTests.scala
@@ -294,7 +294,7 @@ class PluginsFunctionalityTests extends FunSpec with Matchers with Helpers with 
             spinCheck(100 milliseconds,true)(
               Http.post(_sp, spPostBody, textPlain)){resp => val body = new String(resp.payload, "UTF-8").trim
 
-              leaves.forall(l => body.contains(l) == true)
+              leaves.forall(l => body.contains(l))
 
             }.map {
               resp =>
