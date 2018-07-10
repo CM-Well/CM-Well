4287dd68bd7cd25e8448bd19ba60cfeea33e121d
MoriaYechiel <38179268+MoriaYechiel@users.noreply.github.com>
2018-07-10 12:00:22 +0300

    Remove asterisks grep from scripts (#793)

--- server/cmwell-cons/app/cmwell-pe.sh
+++ server/cmwell-cons/app/cmwell-pe.sh
@@ -7,4 +7,4 @@ SYN_LINK_LIB=${4-true}
 cd $(dirname -- "$0")
 MSG='"in script after install"'
 
-bash -c """scala "-Dlocation=$INST_DIR" -nc -cp 'cons-lib/*' -I pe -e 'val newPe = pe.copy(nbg = true, oldBg = false,newBg = true,useAuthorization = $USE_AUTHORIZATION, deployJava = $DEPLOY_JAVA, symLinkLib = $SYN_LINK_LIB); newPe.LogLevel.debug ; newPe.verbose = true ; newPe.install; sys.exit(0)' 2> >( grep -v '^#.*#' )"""
+bash -c """scala "-Dlocation=$INST_DIR" -nc -cp 'cons-lib/*' -I pe -e 'val newPe = pe.copy(nbg = true, oldBg = false,newBg = true,useAuthorization = $USE_AUTHORIZATION, deployJava = $DEPLOY_JAVA, symLinkLib = $SYN_LINK_LIB); newPe.LogLevel.debug ; newPe.verbose = true ; newPe.install; sys.exit(0)'"""
--- server/cmwell-cons/app/logviewer.sh
+++ server/cmwell-cons/app/logviewer.sh
@@ -6,4 +6,4 @@ LOGPATH=$3
 
 cd $(dirname -- "$0")
 source ./cmw.env
-bash -c "scala -cp components/cmwell-controller-assembly-1.2.1-SNAPSHOT.jar:components/cmwell-cons_2.11-1.2.1-SNAPSHOT.jar -I $CLUSTER -e $CLUSTER.getLogViewer(\"$MACHINE\", \"$LOGPATH\") 2> >( grep -v '^#.*#' )"
+bash -c "scala -cp components/cmwell-controller-assembly-1.2.1-SNAPSHOT.jar:components/cmwell-cons_2.11-1.2.1-SNAPSHOT.jar -I $CLUSTER -e $CLUSTER.getLogViewer(\"$MACHINE\", \"$LOGPATH\")
