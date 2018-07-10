3adda5bb37524a988ea0015d138707a32673ebb3
MoriaYechiel <38179268+MoriaYechiel@users.noreply.github.com>
2018-07-10 12:01:35 +0300

    Update scripts with new Java and Scala (#807)

--- server/cmwell-cons/app/extract-java-scala.sh
+++ server/cmwell-cons/app/extract-java-scala.sh
@@ -2,9 +2,9 @@
 cd $(dirname -- "$0")
 bash -c "
 cd components-extras;
-tar -xf scala-2.12.4.tgz;
-tar -xf jdk-8u66-linux-x64.tar.gz;
-mv jdk1.8.0_66 java;
-mv scala-2.12.4 scala;
+tar -xf scala-2.12.6.tgz;
+tar -xf jdk-8u171-linux-x64.tar.gz;
+mv jdk1.8.0_171 java;
+mv scala-2.12.6 scala;
 
 " 2> /dev/null
--- server/cmwell-cons/app/get-3rd-components.sh
+++ server/cmwell-cons/app/get-3rd-components.sh
@@ -10,7 +10,7 @@ rm kibana-4.1.1-linux-x64.tar.gz;
 
 wget http://builder/logstash/logstash-2.1.1.tar.gz;
 wget http://builder/logstash/kibana-4.1.1-linux-x64.tar.gz;
-wget http://downloads.lightbend.com/scala/2.12.4/scala-2.12.4.tgz;
-wget http://builder/java/jdk-8u66-linux-x64.tar.gz;
+wget https://downloads.lightbend.com/scala/2.12.6/scala-2.12.6.tgz;
+wget http://builder/java/jdk-8u171-linux-x64.tar.gz;
 
 cd -"
