1b81fb577b1f66436a6a67971aaacd5c713efe18
Moria Yechiel <moria.yechiel@thomsonreuters.com>
2018-07-10 11:20:11 +0300

    After code review

--- server/cmwell-cons/app/prep.sh
+++ server/cmwell-cons/app/prep.sh
@@ -2,4 +2,4 @@
 cd $(dirname -- "$0")
 source ./cmw.env
 
-scala -nc -cp 'cons-lib/*' -I cmw.conf -e "cmw.prepareMachines(); sys.exit(0)"
+scala -nc -cp 'cons-lib/*' -I cmw.conf -e "cmw.prepareMachines(); sys.exit(0)" 2>&1 | bash -c 'tee > out.log
