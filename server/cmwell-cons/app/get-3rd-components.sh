f368ca57b744231842b285980482e099146bd183
Moria Yechiel <moria.yechiel@thomsonreuters.com>
2018-07-16 12:00:44 +0300

    Change java version to 172

--- server/cmwell-cons/app/get-3rd-components.sh
+++ server/cmwell-cons/app/get-3rd-components.sh
@@ -11,6 +11,6 @@ rm kibana-4.1.1-linux-x64.tar.gz;
 wget http://builder/logstash/logstash-2.1.1.tar.gz;
 wget http://builder/logstash/kibana-4.1.1-linux-x64.tar.gz;
 wget https://downloads.lightbend.com/scala/2.12.6/scala-2.12.6.tgz;
-wget http://builder/java/jdk-8u171-linux-x64.tar.gz;
+wget http://builder/java/jre-8u172-linux-x64.tar.gz
 
 cd -"
