100eefbe34058c92d08bde8bb86777fcf4cfdea3
MoriaYechiel <38179268+MoriaYechiel@users.noreply.github.com>
2018-07-25 18:02:10 +0300

    Remove Kibana and Logstash from get-3rd-components script (#833)

--- server/cmwell-cons/app/get-3rd-components.sh
+++ server/cmwell-cons/app/get-3rd-components.sh
@@ -3,13 +3,9 @@ bash -c "
 
 mkdir -p components-extras;
 cd components-extras;
-rm logstash-2.1.1.tar.gz;
 rm -rf scala-2.11.*.tgz;
 rm -rf jdk*;
-rm kibana-4.1.1-linux-x64.tar.gz;
 
-wget http://builder/logstash/logstash-2.1.1.tar.gz;
-wget http://builder/logstash/kibana-4.1.1-linux-x64.tar.gz;
 wget https://downloads.lightbend.com/scala/2.12.6/scala-2.12.6.tgz;
 wget http://builder/java/jdk-8u172-linux-x64.tar.gz
 
