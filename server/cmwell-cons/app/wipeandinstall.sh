b45ec58edcc8323d6c118619fb943fe286c289ce
Moria Yechiel <moria.yechiel@thomsonreuters.com>
2018-07-31 14:40:28 +0300

    Fix syntax for tee command

--- server/cmwell-cons/app/wipeandinstall.sh
+++ server/cmwell-cons/app/wipeandinstall.sh
@@ -3,6 +3,6 @@
 
 cd $(dirname -- "$0")
 source ./cmw.env
-scala -cp 'cons-lib/*' -I cmw.conf -e "cmw.install; cmw.shutDownDataInitializer(); sys.exit(0)" 2>&1 | bash -c 'tee > out.log'
+scala -cp 'cons-lib/*' -I cmw.conf -e "cmw.install; cmw.shutDownDataInitializer(); sys.exit(0)" 2>&1 | bash -c 'tee out.log'
 
 }
