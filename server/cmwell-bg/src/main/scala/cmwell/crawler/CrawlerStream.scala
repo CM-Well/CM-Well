3b70cfa4566ac12e37fc8b2a22f274ac7d4082b5
Yaakov Breuer <Yaakov.Breuer@thomsonreuters.com>
2018-08-01 16:03:44 +0300

    DCC Fix: Duplicates

--- server/cmwell-bg/src/main/scala/cmwell/crawler/CrawlerStream.scala
+++ server/cmwell-bg/src/main/scala/cmwell/crawler/CrawlerStream.scala
@@ -15,27 +15,31 @@
 
 package cmwell.crawler
 
+import java.util.Properties
+
 import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
-import akka.kafka.scaladsl.Consumer
-import akka.kafka.{ConsumerSettings, Subscriptions}
+import akka.kafka.ProducerMessage.Message
+import akka.kafka.scaladsl.{Consumer, Producer}
+import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
 import akka.stream.{ActorMaterializer, ClosedShape, SourceShape}
 import akka.stream.scaladsl.{GraphDSL, RunnableGraph, Sink, Source}
 import cmwell.common._
-import cmwell.common.formats.BGMessage
-import cmwell.fts.{FTSServiceNew, FTSServiceOps}
+import cmwell.common.formats.{BGMessage, Offset}
+import cmwell.fts.{FTSServiceNew, FTSServiceOps, FTSThinInfoton}
 import cmwell.irw.IRWService
 import cmwell.zstore.ZStore
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
 import org.apache.kafka.common.TopicPartition
-import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
+import org.apache.kafka.common.serialization.{ByteArrayDeserializer, ByteArraySerializer, StringDeserializer}
 
 import scala.concurrent.duration.{DurationInt, DurationLong, FiniteDuration}
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success, Try}
-import cmwell.util.concurrent.travector
+import cmwell.util.concurrent.{timeoutFuture, travector}
+import org.apache.kafka.clients.producer._
 
 //case class CrawlerPosition(offset: Long, timeStamp: Long)
 sealed trait CrawlerState
@@ -53,19 +57,28 @@ case class PathsVersions(latest: Option[CasVersion], versions: Vector[CasVersion
 case class KafkaLocation(topic: String, partition: Int, offset: Long) {
   override def toString: String = s"[$topic, partition: $partition, offset: $offset]"
 }
-case class CommandWithLocation(cmd: SingleCommand, location: KafkaLocation)
+case class LocalizedCommand(cmd: SingleCommand, location: KafkaLocation)
+
 sealed trait DetectionResult
+
 //All is ok and there is nothing more to check
-case class AllClear(cmdWithLocation: CommandWithLocation) extends DetectionResult
+case class AllClear(lclzdCmd: LocalizedCommand) extends DetectionResult
+
 //so far it's ok - continue checking
-case class SoFarClear(pathVersions: PathsVersions, cmdWithLocation: CommandWithLocation) extends DetectionResult
-sealed trait DetectionError {
+case class SoFarClear(pathVersions: PathsVersions, lclzdCmd: LocalizedCommand) extends DetectionResult
+sealed trait DetectionError extends DetectionResult {
   val details: String
-  val cmdWithLocation: CommandWithLocation
+  val lclzdCmd: LocalizedCommand
 }
-case class CasError(details: String, cmdWithLocation: CommandWithLocation) extends DetectionResult with DetectionError
-case class EsBadCurrentError(details: String, cmdWithLocation: CommandWithLocation) extends DetectionResult with DetectionError
-case class EsMissingUuidError(details: String, cmdWithLocation: CommandWithLocation) extends DetectionResult with DetectionError
+
+case class EsRecord(thinfoton: FTSThinInfoton, isCurrent: Boolean, indexName: String)
+
+case class CasError(details: String, lclzdCmd: LocalizedCommand) extends DetectionResult with DetectionError
+case class EsBadCurrentError(esRecord: EsRecord, details: String, lclzdCmd: LocalizedCommand) extends DetectionResult with DetectionError
+case class EsMissingUuidError(details: String, lclzdCmd: LocalizedCommand) extends DetectionResult with DetectionError
+
+sealed trait Fix extends DetectionResult with DetectionError
+case class EsBadCurrentErrorFix(details: String, lclzdCmd: LocalizedCommand) extends Fix
 
 object CrawlerStream extends LazyLogging {
   case class CrawlerMaterialization(control: Consumer.Control, doneState: Future[Done])
@@ -88,15 +101,35 @@ object CrawlerStream extends LazyLogging {
     val retryDuration = config.getDuration("cmwell.crawler.retryDuration").getSeconds.seconds
     val checkParallelism = config.getInt("cmwell.crawler.checkParallelism")
 
+    // TODO: Do we want to produce Kafka messages (e.g. to index_topic) via Flow?
+//    val kafkaProducerFlow = {
+//      val baSerializer = new ByteArraySerializer()
+//      val kafkaProducerSettings = {
+//        ProducerSettings(sys, baSerializer, baSerializer).
+//          withBootstrapServers(bootStrapServers).
+//          withProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "lz4")
+//      }
+//      Producer.flow[Array[Byte], Array[Byte], Seq[Offset]](kafkaProducerSettings).map(_.message.passThrough)
+//    }
+    val kafkaProducer = {
+      val producerProperties = new Properties
+      producerProperties.put("bootstrap.servers", bootStrapServers)
+      producerProperties.put("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+      producerProperties.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+      producerProperties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "lz4")
+      new KafkaProducer[Array[Byte], Array[Byte]](producerProperties)
+    }
+
     val initialPersistOffset = Try(offsetsService.readWithTimestamp(persistId))
 
-    def checkKafkaMessage(msg: ConsumerRecord[Array[Byte], Array[Byte]]): Future[Long] = {
+    def checkAndFixKafkaMessage(msg: ConsumerRecord[Array[Byte], Array[Byte]]): Future[Long] = {
       kafkaMessageToSingleCommand(msg)
         .flatMap(getVersionsFromPathsTable)(ec)
         .flatMap(checkInconsistencyOfPathTableOnly)(ec)
         .flatMap(enrichVersionsWithSystemFields)(ec)
         .map(checkSystemFields)(ec)
         .flatMap(checkEsVersions)(ec)
+        .flatMap(fixEsCurrentState)(ec)
         .map(reportErrors)(ec)
         .map(_ => msg.offset())(ec)
     }
@@ -104,12 +137,12 @@ object CrawlerStream extends LazyLogging {
     def kafkaMessageToSingleCommand(msg: ConsumerRecord[Array[Byte], Array[Byte]]) = {
       val kafkaLocation = KafkaLocation(topic, partition, msg.offset())
       CommandSerializer.decode(msg.value) match {
-        case CommandRef(ref) => zStore.get(ref).map(cmd => CommandWithLocation(CommandSerializer.decode(cmd).asInstanceOf[SingleCommand], kafkaLocation))(ec)
-        case singleCommand => Future.successful(CommandWithLocation(singleCommand.asInstanceOf[SingleCommand], kafkaLocation))
+        case CommandRef(ref) => zStore.get(ref).map(cmd => LocalizedCommand(CommandSerializer.decode(cmd).asInstanceOf[SingleCommand], kafkaLocation))(ec)
+        case singleCommand => Future.successful(LocalizedCommand(singleCommand.asInstanceOf[SingleCommand], kafkaLocation))
       }
     }
 
-    def getVersionsFromPathsTable(cmdOffset: CommandWithLocation) = {
+    def getVersionsFromPathsTable(cmdOffset: LocalizedCommand) = {
       implicit val localEc: ExecutionContext = ec
       val cmd = cmdOffset.cmd
       val latestVersion = irwService.lastVersion(cmd.path).map(v1 => v1.map(v2 => BareCasVersion(v2._2, v2._1)))(ec)
@@ -125,10 +158,10 @@ object CrawlerStream extends LazyLogging {
     }
 
     //checks that the given command is ok (either in paths table or null update or grouped command)
-    def checkInconsistencyOfPathTableOnly(pathsCmdWithLocation: (PathsVersions, CommandWithLocation)) = {
-      val (paths, cmdWithLocation@CommandWithLocation(cmd, location)) = pathsCmdWithLocation
+    def checkInconsistencyOfPathTableOnly(pathslclzdCmd: (PathsVersions, LocalizedCommand)) = {
+      val (paths, lclzdCmd@LocalizedCommand(cmd, location)) = pathslclzdCmd
       if (paths.latest.isEmpty)
-        Future.successful[DetectionResult](CasError(s"No last version in paths table for path ${cmd.path}!", cmdWithLocation))
+        Future.successful[DetectionResult](CasError(s"No last version in paths table for path ${cmd.path}!", lclzdCmd))
       else {
         //in case initial version of an infoton that was written several times fast, there will be a grouped commands without anything before.
         //Crawler needs to check whether this is the case (e.g. empty versions and the command is grouped)
@@ -137,13 +170,13 @@ object CrawlerStream extends LazyLogging {
           zStore.getStringOpt(s"imp.${partitionId}_${location.offset}").map {
             //the current offset was null update of grouped command. Bg didn't change anything in the system due to it - The check is finished in this stage
             case Some("nu" | "grp") =>
-              AllClear(cmdWithLocation)
+              AllClear(lclzdCmd)
             case _ => CasError(s"command [path:${cmd.path}, last modified:${cmd.lastModified}] " +
-              s"is not in paths table and it isn't null update or grouped command!", cmdWithLocation)
+              s"is not in paths table and it isn't null update or grouped command!", lclzdCmd)
           }(ec)
         }
         //This offset's command exists. Still need to verify current and ES.
-        else Future.successful(SoFarClear(paths, cmdWithLocation))
+        else Future.successful(SoFarClear(paths, lclzdCmd))
       }
     }
 
@@ -153,12 +186,12 @@ object CrawlerStream extends LazyLogging {
 
     def enrichVersionsWithSystemFields(previousResult: DetectionResult) = {
       previousResult match {
-        case SoFarClear(PathsVersions(latestOpt, versions), cmdWithLocation) =>
+        case SoFarClear(PathsVersions(latestOpt, versions), lclzdCmd) =>
           //todo: I am not sure getting the fields of latest is needed
           //val latest = latestOpt.get
           //val enrichedLatestFut = getSystemFields(latest.uuid).map(CasVersionWithSysFields(latest.uuid, latest.timestamp, _))
           val enrichedVersionsFut = travector(versions)(v => getSystemFields(v.uuid).map(CasVersionWithSysFields(v.uuid, v.timestamp, _))(ec))(ec)
-          enrichedVersionsFut.map(enrichedVersions => SoFarClear(PathsVersions(latestOpt, enrichedVersions), cmdWithLocation))(ec)
+          enrichedVersionsFut.map(enrichedVersions => SoFarClear(PathsVersions(latestOpt, enrichedVersions), lclzdCmd))(ec)
         //for (l <- enrichedLatestFut; v <- enrichedVersionsFut) yield SoFarClear(PathsVersions(Some(l), v), cmdAndOffset)
         case other => Future.successful(other)
       }
@@ -166,22 +199,22 @@ object CrawlerStream extends LazyLogging {
 
     def checkSystemFields(previousResult: DetectionResult) = {
       previousResult match {
-        case prev@SoFarClear(PathsVersions(latestOpt, versions), cmdWithLocation) =>
+        case prev@SoFarClear(PathsVersions(latestOpt, versions), lclzdCmd) =>
           //The assumption is that the only version checked is the actual version written now.
           //(BG changes only ES current and nothing in CAS for previous versions)
           val analyzed = versions.head
           analyzed match {
             case CasVersionWithSysFields(uuid, timestamp, fields) =>
-              logger.trace(s"$crawlerId The fields of [offset: ${cmdWithLocation.location.offset}, uuid: $uuid, timestamp: $timestamp] are: $fields")
+              logger.trace(s"$crawlerId The fields of [offset: ${lclzdCmd.location.offset}, uuid: $uuid, timestamp: $timestamp] are: $fields")
               val badFields = fields.groupBy(_.name).collect {
                 case (name, values) if values.length > 1 => s"field [$name] has too many values [${values.map(_.value).mkString(",")}]"
               }
               val analyzedFields = fields.map(_.name).toSet
               val missingFields = systemFieldsNames.filterNot(analyzedFields)
               if (missingFields.nonEmpty)
-                CasError(s"system fields [${missingFields.mkString(",")}] are missing!", cmdWithLocation)
+                CasError(s"system fields [${missingFields.mkString(",")}] are missing!", lclzdCmd)
               else if (badFields.nonEmpty) {
-                CasError(s"Duplicated system fields! ${badFields.mkString(";")}", cmdWithLocation)
+                CasError(s"Duplicated system fields! ${badFields.mkString(";")}", lclzdCmd)
               }
               else prev
             case _ => ??? //should never happen
@@ -193,8 +226,9 @@ object CrawlerStream extends LazyLogging {
     def verifyEsCurrentState(uuid: String, indexName: String, shouldBeCurrent: Boolean)(previous: SoFarClear) =
       ftsService.get(uuid, indexName)(ec).map {
         case Some((_, isActualCurrent)) if isActualCurrent == shouldBeCurrent => previous
-        case Some((_, isActualCurrent)) => EsBadCurrentError(s"uuid [$uuid] has unexpected current property of [$isActualCurrent]", previous.cmdWithLocation)
-        case None => EsMissingUuidError(s"uuid $uuid doesn't exist in index $indexName.", previous.cmdWithLocation)
+        case Some((ti, isActualCurrent)) => EsBadCurrentError(EsRecord(ti, isActualCurrent, indexName),
+          s"uuid [$uuid] has unexpected current property of [$isActualCurrent]", previous.lclzdCmd)
+        case None => EsMissingUuidError(s"uuid $uuid doesn't exist in index $indexName.", previous.lclzdCmd)
       }(ec)
 
     def checkFirstEsVersion(first: CasVersionWithSysFields)(previousResult: SoFarClear) = {
@@ -230,12 +264,30 @@ object CrawlerStream extends LazyLogging {
       }
     }
 
+    def sendIndexExistingCommand(thinfoton: FTSThinInfoton, indexName: String, lclzdCmd: LocalizedCommand): Future[Unit] = {
+      val cmd = lclzdCmd.cmd
+      val indexTopic = if(topic.contains("priority")) "index_topic.priority" else "index_topic"
+      val command = IndexExistingInfotonCommandForIndexer(thinfoton.uuid, 0L, thinfoton.path, indexName, Seq.empty, Seq.empty)
+      val payloadForKafka: Array[Byte] = CommandSerializer.encode(command)
+      val pRecord = new ProducerRecord[Array[Byte], Array[Byte]](indexTopic, cmd.path.getBytes("UTF-8"), payloadForKafka)
+      injectKafkaFuture(kafkaProducer.send(pRecord, _))(ec).map(_ => ())(ec)
+    }
+
+    def fixEsCurrentState(previousResult: DetectionResult): Future[DetectionResult] = previousResult match {
+      case EsBadCurrentError(EsRecord(thinfoton, isCurrent, indexName), details, lclzdCmd) if isCurrent =>
+        sendIndexExistingCommand(thinfoton, indexName, lclzdCmd).map(_ => EsBadCurrentErrorFix(details, lclzdCmd))(ec)
+      case _ => Future.successful(previousResult)
+    }
+
     def reportErrors(finalResult: DetectionResult) = {
       finalResult match {
+        case fix: EsBadCurrentErrorFix =>
+          logger.error(s"$crawlerId Inconsistency found: ${fix.details} for command in location ${fix.lclzdCmd.location}. " +
+            s"Original command: ${fix.lclzdCmd.cmd} and was fixed.")
         case err: DetectionError =>
-          logger.error(s"$crawlerId Inconsistency found: ${err.details} for command in location ${err.cmdWithLocation.location}. " +
-            s"Original command: ${err.cmdWithLocation.cmd}")
-        case other => other
+          logger.error(s"$crawlerId Inconsistency found: ${err.details} for command in location ${err.lclzdCmd.location}. " +
+            s"Original command: ${err.lclzdCmd.cmd}")
+        case _ => /* Do nothing */
       }
     }
 
@@ -252,7 +304,7 @@ object CrawlerStream extends LazyLogging {
         val nonBackpressuredMessageSrc = messageSource(initialOffset, topic, partition, bootStrapServers)(sys)
         val messageSrc = backpressuredMessageSource(crawlerId, offsetSrc, nonBackpressuredMessageSrc)
         messageSrc
-          .mapAsync(checkParallelism)(checkKafkaMessage)
+          .mapAsync(checkParallelism)(checkAndFixKafkaMessage)
           .via(CrawlerRatePrinter(crawlerId, 500, 60000)(logger))
           //todo: We need only the last element. There might be a way without save all the elements. Also getting last can be time consuming
           .groupedWithin(maxAmount, maxTime)
@@ -346,4 +398,12 @@ object CrawlerStream extends LazyLogging {
         }
       }
     })
+
+  private def injectKafkaFuture(future: Callback => java.util.concurrent.Future[RecordMetadata],
+    timeout: FiniteDuration = 9.seconds)(implicit ec: ExecutionContext) = {
+    val p = Promise[RecordMetadata]()
+    future((metadata: RecordMetadata, exception: Exception) =>
+      if (exception != null) p.failure(exception) else p.success(metadata))
+    timeoutFuture(p.future, timeout)(ec)
+  }
 }
