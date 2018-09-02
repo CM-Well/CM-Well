51ce22da3cb824d511ea8cd22e40da2ac2b0b0a4
Eli Orzitzer <Eli.Orzitzer@thomsonreuters.com>
2018-09-02 16:25:40 +0300

    crawler: properly ignore commands with plus one added

--- server/cmwell-bg/src/main/scala/cmwell/crawler/CrawlerStream.scala
+++ server/cmwell-bg/src/main/scala/cmwell/crawler/CrawlerStream.scala
@@ -33,6 +33,7 @@ import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
 import org.elasticsearch.action.ActionRequest
 import org.elasticsearch.action.update.UpdateRequest
+import org.joda.time.DateTime
 
 import scala.concurrent.duration.{DurationInt, DurationLong, FiniteDuration}
 import scala.concurrent.{ExecutionContext, Future}
@@ -54,7 +55,7 @@ case class PathsVersions(latest: Option[CasVersion], versions: Vector[CasVersion
 case class KafkaLocation(topic: String, partition: Int, offset: Long) {
   override def toString: String = s"[$topic, partition: $partition, offset: $offset]"
 }
-case class LocalizedCommand(cmd: SingleCommand, location: KafkaLocation)
+case class LocalizedCommand(cmd: SingleCommand, originalLastModified: Option[DateTime], location: KafkaLocation)
 
 sealed trait DetectionResult
 
@@ -115,8 +116,8 @@ object CrawlerStream extends LazyLogging {
     def kafkaMessageToSingleCommand(msg: ConsumerRecord[Array[Byte], Array[Byte]]) = {
       val kafkaLocation = KafkaLocation(topic, partition, msg.offset())
       CommandSerializer.decode(msg.value) match {
-        case CommandRef(ref) => zStore.get(ref).map(cmd => LocalizedCommand(CommandSerializer.decode(cmd).asInstanceOf[SingleCommand], kafkaLocation))(ec)
-        case singleCommand => Future.successful(LocalizedCommand(singleCommand.asInstanceOf[SingleCommand], kafkaLocation))
+        case CommandRef(ref) => zStore.get(ref).map(cmd => LocalizedCommand(CommandSerializer.decode(cmd).asInstanceOf[SingleCommand], None, kafkaLocation))(ec)
+        case singleCommand => Future.successful(LocalizedCommand(singleCommand.asInstanceOf[SingleCommand], None, kafkaLocation))
       }
     }
 
@@ -137,7 +138,7 @@ object CrawlerStream extends LazyLogging {
 
     //checks that the given command is ok (either in paths table or null update or grouped command)
     def checkInconsistencyOfPathTableOnly(pathslclzdCmd: (PathsVersions, LocalizedCommand)) = {
-      val (paths, lclzdCmd@LocalizedCommand(cmd, location)) = pathslclzdCmd
+      val (paths, lclzdCmd@LocalizedCommand(cmd, _, location)) = pathslclzdCmd
       if (paths.latest.isEmpty)
         cmd match {
           case _: DeletePathCommand | _: DeleteAttributesCommand =>
@@ -155,12 +156,26 @@ object CrawlerStream extends LazyLogging {
             //the current offset was null update of grouped command. Bg didn't change anything in the system due to it - The check is finished in this stage
             case Some("nu" | "grp") =>
               AllClear(lclzdCmd)
+            case Some(alteredLastModified) =>
+              val alteredCommand = alterCommandLastModifiedDate(cmd, alteredLastModified.toLong)
+              SoFarClear(paths, LocalizedCommand(alteredCommand, Some(cmd.lastModified), location))
             case _ => CasError(s"command [path:${cmd.path}, last modified:${cmd.lastModified}] " +
               s"is not in paths table and it isn't null update or grouped command!", lclzdCmd)
           }(ec)
         }
         //This offset's command exists. Still need to verify current and ES.
         else Future.successful(SoFarClear(paths, lclzdCmd))
+      }
+    }
+
+    def alterCommandLastModifiedDate(cmd: SingleCommand, newDateTimestamp: Long) = {
+      val newDate = new DateTime(newDateTimestamp)
+      cmd match {
+        case c@WriteCommand(infoton, _, _) => c.copy(infoton = infoton.copyInfoton(lastModified = newDate))
+        case c@DeleteAttributesCommand(_, _, _, _, _) => c.copy(lastModified = newDate)
+        case c@DeletePathCommand(_, _, _, _) => c.copy(lastModified = newDate)
+        case c@UpdatePathCommand(_, _, _, _, _, _) => c.copy(lastModified = newDate)
+        case c@OverwriteCommand(infoton, _) => c.copy(infoton = infoton.copyInfoton(lastModified = newDate))
       }
     }
 
@@ -279,10 +294,10 @@ object CrawlerStream extends LazyLogging {
       finalResult match {
         case fix: EsBadCurrentErrorFix =>
           logger.error(s"$crawlerId Inconsistency found: ${fix.details} for command in location ${fix.lclzdCmd.location}. " +
-            s"Original command: ${fix.lclzdCmd.cmd} and was fixed.")
+            s"Original command: ${fix.lclzdCmd.cmd}${fix.lclzdCmd.originalLastModified.fold("")(olm => s" [Original command date: $olm]")} and was fixed.")
         case err: DetectionError =>
           logger.error(s"$crawlerId Inconsistency found: ${err.details} for command in location ${err.lclzdCmd.location}. " +
-            s"Original command: ${err.lclzdCmd.cmd}")
+            s"Original command: ${err.lclzdCmd.cmd}${err.lclzdCmd.originalLastModified.fold("")(olm => s" [Original command date: $olm]")}")
         case _ => /* Do nothing */
       }
     }
