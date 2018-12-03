7aada76f9e707460e4c528b77cf86d0dea23686e
Naomi Ruppin <ext-naomi.ruppin@here.com>
2018-12-03 17:05:48 -0500

    Add Yak RN.

--- tools/dataConsistencyTool/cmwell-spark-analysis/src/main/scala/cmwell/analytics/data/EstimateDatasetSize.scala
+++
@@ -1,8 +0,0 @@
-package cmwell.analytics.data
-
-import org.apache.spark.sql.SparkSession
-
-/** A trait for Dataset classes that can estimate the size of their Dataset representation (in Tungsten format). */
-trait EstimateDatasetSize {
-  def estimateDatasetSize(implicit spark: SparkSession): Long
-}
--- tools/dataConsistencyTool/cmwell-spark-analysis/src/scripts/spark-shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start a Spark shell (e.g., for ad-hoc analysis on extracted data).
-
-source ./set-runtime.sh
-
-$SPARK_HOME/bin/spark-shell \
- --master "${SPARK_MASTER}" --driver-memory "${SPARK_MEMORY}" --conf "spark.local.dir=${SPARK_TMP}"
