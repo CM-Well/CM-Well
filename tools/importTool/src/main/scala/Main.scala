9777d91d2904d0b6964d40e767268cf0db7bef50
lielShraga <44337999+lielShraga@users.noreply.github.com>
2018-12-05 12:23:41 +0200

    adding an import url tool (#990)
    adding an import url tool

---
+++ tools/importTool/src/main/scala/Main.scala
@@ -0,0 +1,23 @@
+import akka.actor.{ActorSystem, Props}
+import org.rogach.scallop.ScallopConf
+
+
+  class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+    val sourceUrl = opt[String]("source-url", required = true, descr = "the source url which download rdf file")
+    val format = opt[String]("format", required = true, descr="the ofile format")
+    var cluster = opt[String]("cluster", required = true, descr="the target server which content is ingested to")
+    verify()
+  }
+
+  object Main {
+    def main(args: Array[String]) {
+      val conf = new Conf(args)  // Note: This line also works for "object Main extends App"
+      println("source file is: " + conf.sourceUrl())
+      println("output format is: " + conf.format())
+      val system = ActorSystem("MySystem")
+      println("About to Start import tool flow...")
+      val mainActor = system.actorOf(Props(new AkkaFileReaderWithActor(conf.sourceUrl(), conf.format(), conf.cluster())), name = "myactor")
+      mainActor ! ActorInput
+    }
+
+}
