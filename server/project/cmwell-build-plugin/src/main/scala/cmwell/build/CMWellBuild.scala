823dee12a5e8f070753c50cdfbcd8ca37a7c0ce2
Eli Orzitzer <Eli.Orzitzer@thomsonreuters.com>
2019-04-04 16:29:37 +0300

    'per Liel's request improved the comment

--- server/project/cmwell-build-plugin/src/main/scala/cmwell/build/CMWellBuild.scala
+++ server/project/cmwell-build-plugin/src/main/scala/cmwell/build/CMWellBuild.scala
@@ -194,6 +194,7 @@ object CMWellBuild extends AutoPlugin {
 				"SHA-1" -> (url + ".sha1"),
 				"SHA-256" -> (url + ".sha256")
 				//SHA-512 is disabled until https://github.com/coursier/coursier/issues/1108 will be fixed
+				//(coursier parser doesn't recognize sha512 hash length, see above issue)
 				//This means that fetching ES currently doesn't have any checksum check!
 				//"SHA-512" -> (url + ".sha512")
 			),
