55874065ecd8fed6a5e544a413f51510a72ebfdf
Nick Lester <nickoslester@gmail.com>
2019-05-23 16:27:14 +0100

    Removed comments

--- server/cmwell-cons/script.sh
+++ server/cmwell-cons/script.sh
@@ -8,14 +8,8 @@ export HAL=9000
 
 sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/es/cur/start-master.sh
 sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/ctrl/start.sh
-#sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/cas/cur/start.sh
-#sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/es/cur/start.sh
-#sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/kafka/start.sh
-#sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/zookeeper/start.sh
+
 sleep 10
-#sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/bg/start.sh
-#sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/ws/start.sh
-#sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/ws/cw-start.sh
 sudo -u u HAL=9000 PATH=/home/u/app/cmwell/app/java/bin:$PATH /home/u/app/cm-well/app/dc/start.sh
 " &
 
