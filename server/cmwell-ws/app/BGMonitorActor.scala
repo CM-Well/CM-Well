b8b8ecb8f46f10b11e7e68ca6988bea8a0e53468
Moria Yechiel <moria.yechiel@thomsonreuters.com>
2019-05-28 10:39:12 +0300

    add volotile

--- server/cmwell-ws/app/BGMonitorActor.scala
+++ server/cmwell-ws/app/BGMonitorActor.scala
@@ -4,7 +4,7 @@
   * Licensed under the Apache License, Version 2.0 (the “License”); you may not use this file except in compliance with the License.
   * You may obtain a copy of the License at
   *
-  * http://www.apache.org/licenses/LICENSE-2.0
+  *   http://www.apache.org/licenses/LICENSE-2.0
   *
   * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
   * an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -86,7 +86,7 @@ class BGMonitorActor(zkServers: String,
 
   val topicsPartitions = topicsPartitionsAndConsumers.keys
 
-  var previousOffsetInfo: OffsetsInfo = OffsetsInfo(Map.empty[String, PartitionOffsetsInfo], DateTime.now())
+  @volatile var previousOffsetInfo: OffsetsInfo = OffsetsInfo(Map.empty[String, PartitionOffsetsInfo], DateTime.now())
   @volatile var currentOffsetInfo: OffsetsInfo = OffsetsInfo(Map.empty[String, PartitionOffsetsInfo], DateTime.now())
 
   @volatile var lastFetchDuration: Long = 30L
