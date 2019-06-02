1df91726eda21225b9bd897b822f46cf62ce2e67
Eli Orzitzer <Eli.Orzitzer@thomsonreuters.com>
2019-06-02 18:48:00 +0300

    dc-sync: add transformations support to monitor

--- server/cmwell-controller/src/main/scala/cmwell/ctrl/checkers/DcChecker.scala
+++ server/cmwell-controller/src/main/scala/cmwell/ctrl/checkers/DcChecker.scala
@@ -28,7 +28,7 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import cmwell.util.http.{SimpleHttpClient => Http}
 
-case class ActiveDcSync(id: String, qp: Option[String], wh: Boolean, host: String)
+case class ActiveDcSync(id: String, qp: Option[String], wh: Boolean, transformations: List[String], host: String)
 case class InfotonDiff(me: Long, remote: Long) {
   def isEqual = me == remote
 }
@@ -61,8 +61,9 @@ object DcChecker extends Checker with RestarterChecker with LazyLogging {
         val id = fields.\("id").\(0).as[String]
         val qp = fields.\("qp").\(0).toOption.flatMap(_.asOpt[String])
         val wh = fields.\("with-history").\(0).toOption.flatMap(_.asOpt[String]).getOrElse("true")
+        val transformations = fields.\("transformations").asOpt[List[String]].getOrElse(List.empty)
         val location = fields.\("location").\(0).as[String]
-        ActiveDcSync(id, qp, wh == "true", location)
+        ActiveDcSync(id, qp, wh == "true", transformations, location)
       }
     }
   }
@@ -98,6 +99,10 @@ object DcChecker extends Checker with RestarterChecker with LazyLogging {
       .map(jvm => Grid.selectActor(ClientActor.name, jvm) ! RestartJvm)
   }
 
+  private def transform(transformations: List[(String, String)], str: String): String = {
+    transformations.foldLeft(str)((result, kv) => result.replace(kv._1, kv._2))
+  }
+
   override def check: Future[ComponentState] = {
     val host = s"${Config.webAddress}:${Config.webPort}"
     val result = getActiveDcSyncs(host).flatMap { activeDcSync =>
@@ -105,13 +110,15 @@ object DcChecker extends Checker with RestarterChecker with LazyLogging {
         val qpStr = dc.qp.fold("")(qp => s"qp=$qp")
         val whStr = if (dc.wh) "with-history" else ""
         val qpAndWhStr = (for (str <- List(qpStr, whStr) if str.nonEmpty) yield str).mkString("&")
+        val t = dc.transformations.map(_.split("->") match { case Array(source, target) => (source, target)})
         val qpAndWhStrFinal = if (qpAndWhStr.length == 0) "" else "?" + qpAndWhStr
-        val id = s"${dc.id}$qpAndWhStrFinal"
+        val transStr = if (dc.transformations.isEmpty) "" else s"&trans:${dc.transformations.mkString("[", ",", "]")}"
+        val id = s"${dc.id}$qpAndWhStrFinal$transStr"
         val remoteHost = dc.host
         val remoteLastIndexTimeF = getLastIndexTime(remoteHost, dc.id, dc.qp, dc.wh).recover {
           case err: Throwable => -1L
         }
-        val localLastIndexTimeF = getLastIndexTime(host, dc.id, dc.qp, dc.wh).recover { case err: Throwable => -1L }
+        val localLastIndexTimeF = getLastIndexTime(host, dc.id, dc.qp.map(transform(t, _)), dc.wh).recover { case err: Throwable => -1L }
         val aggregated = for {
           remoteLastIndexTime <- remoteLastIndexTimeF
           localLastIndexTime <- localLastIndexTimeF
@@ -126,9 +133,7 @@ object DcChecker extends Checker with RestarterChecker with LazyLogging {
               getLastStates(1).headOption match {
                 case Some(sb: DcStatesBag) =>
                   val lastState = sb.states.get(id)
-                  logger.debug(
-                    s"DcChecker: Last state was $lastState, remote index time: ${dcDiff.indextimeDiff.remote}"
-                  )
+                  logger.debug(s"DcChecker: Last state was $lastState, remote index time: ${dcDiff.indextimeDiff.remote}")
                   val newState = lastState match {
                     case Some(DcSyncing(dcId, _dcDiff, ch, _)) =>
                       if (dcDiff.indextimeDiff.isEqual) DcSyncing(id, dcDiff, Config.listenAddress)
