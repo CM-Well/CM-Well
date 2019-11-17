321c98463c19568a4aeb0e1bc044974d95d9dc0e
Moria Yechiel <moria.yechiel@thomsonreuters.com>
2019-11-17 08:51:30 +0200

    Remove multiple disks flag

--- server/cmwell-cons/src/main/scala/ComponentConf.scala
+++ server/cmwell-cons/src/main/scala/ComponentConf.scala
@@ -225,7 +225,7 @@ case class ElasticsearchConf(clusterName: String,
                              rs: RackSelector,
                              g1: Boolean,
                              hostIp: String,
-                             dirsPerEs: Option[Int] = None)
+                             dirsPerEs: Int = 1)
     extends ComponentConf(hostIp, s"$home/app/es/cur", sName, s"$home/conf/$dir", "elasticsearch.yml", index) {
 
   val classpath = s"""'$home/app/es/cur/lib/*:'"""
@@ -272,7 +272,9 @@ case class ElasticsearchConf(clusterName: String,
       "transport_port" -> transportPort.toString,
       "num_of_shards" -> expectedNodes.toString,
       "num_of_replicas" -> { if (expectedNodes > 2) 2 else 0 }.toString,
-      "path_data" -> dirsPerEs.fold(s"$home/data/$dir"){x => (2 to x).map(_.toString).fold(s"$home/data/es")((acc, x) => s"$home/data/es$x,$acc")}
+      "path_data" -> { if (dirsPerEs == 1) s"$home/data/$dir"
+                       else {
+                         (2 to dirsPerEs).map(_.toString).fold(s"$home/data/es")((acc, x) => s"$home/data/es$x,$acc")}}
     )
 
     val confContent = ResourceBuilder.getResource(s"scripts/templates/$template", m)
