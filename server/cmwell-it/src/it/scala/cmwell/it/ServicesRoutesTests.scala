1186f62e1f0b87a1d7cc2659dd01eb8ef9fb944d
Yaakov Breuer <Yaakov.Breuer@thomsonreuters.com>
2019-11-27 15:32:49 +0200

    Fixing bug in ServiceRoute removal

--- server/cmwell-it/src/it/scala/cmwell/it/ServicesRoutesTests.scala
+++ server/cmwell-it/src/it/scala/cmwell/it/ServicesRoutesTests.scala
@@ -16,16 +16,26 @@
 package cmwell.it
 
 import com.typesafe.scalalogging.LazyLogging
-import org.scalatest.{AsyncFunSpec, EitherValues, Inspectors, Matchers, OptionValues}
+import org.scalatest.{AsyncFunSpec, Matchers}
+
 import scala.concurrent.duration.DurationInt
 
 class ServicesRoutesTests extends AsyncFunSpec with Matchers with Helpers with LazyLogging {
 
   import cmwell.util.http.SimpleResponse.Implicits.UTF8StringHandler
 
+  private def putTextualFileInfoton(path: String, payload: String) = {
+    Http.post(path, payload, Some("text/plain"),
+      headers = tokenHeader :+ "X-CM-Well-Type" -> "File").flatMap { _ =>
+      spinCheck(100.millis, true)(Http.get(path))(_.status == 200)
+    }
+  }
+
+  private def refreshCache() = Http.get(cmw / "_services-cache", List("op" -> "refresh"))
+
   describe("Services Routes") {
     val ingestServiceInfotonsAndRefreshCache = {
-      val totalServices = 1
+      val totalServices = 2
       val data =
         """@prefix nn:    <cmwell://meta/nn#> .
           |@prefix sys:   <cmwell://meta/sys#> .
@@ -34,12 +44,20 @@ class ServicesRoutesTests extends AsyncFunSpec with Matchers with Helpers with L
           |o:PermID  a                 sys:Redirection ;
           |        nn:route            "/permid/" ;
           |        nn:sourcePattern    "/permid/(.*)" ;
-          |        nn:replacement      "/graph.link/PID-$1" .""".stripMargin
+          |        nn:replacement      "/graph.link/PID-$1" .
+          |
+          |
+          |o:TempRoute  a              sys:Redirection ;
+          |        nn:route            "/temp/" ;
+          |        nn:sourcePattern    "/temp/(.*)" ;
+          |        nn:replacement      "/graph.link/temp/$1" .
+          |
+          |        """.stripMargin
 
 
       Http.post(_in, data, Some("text/rdf+turtle;charset=UTF-8"), List("format" -> "ttl"), tokenHeader).flatMap { _ =>
         spinCheck(100.millis, true)(Http.get(cmw / "meta" / "services", List("op" -> "stream")))(_.payload.trim().lines.size == totalServices)
-      }.flatMap(_ => Http.get(cmw / "_services-cache", List("op" -> "refresh")))
+      }.flatMap(_ => refreshCache())
     }
 
     it("Should get an Infoton via Service Route of Redirection type") {
@@ -47,14 +65,30 @@ class ServicesRoutesTests extends AsyncFunSpec with Matchers with Helpers with L
 
       val payload = "Hello World 789"
       val actualPath = cmw / "graph.link" / "PID-789"
-
-      val ingestTargetInfoton = Http.post(actualPath, payload, Some("text/plain"),
-        headers = tokenHeader :+ "X-CM-Well-Type" -> "File").flatMap { _ =>
-        spinCheck(100.millis, true)(Http.get(actualPath))(_.status == 200)
-      }
+      val ingestTargetInfoton = putTextualFileInfoton(actualPath, payload)
 
       (ingestTargetInfoton zip ingestServiceInfotonsAndRefreshCache).flatMap { _ =>
         Http.get(cmw / "permid" / "789").map(_.payload should be(payload))
+      }
+    }
+
+    it("Should remove a service route") {
+      val tempRouteServiceInfotonPath = cmw / "meta" / "services" / "TempRoute"
+      val virtualPath = cmw / "temp" / "1234"
+      val actualPath = cmw / "graph.link" / "temp" / "1234"
+
+      val ingestTargetInfoton = putTextualFileInfoton(actualPath, payload = "Foo Bar")
+
+      (ingestTargetInfoton zip ingestServiceInfotonsAndRefreshCache).flatMap { _ =>
+        Http.get(virtualPath).map(_.status should be(200))
+      }.flatMap { _ =>
+        Http.delete(tempRouteServiceInfotonPath, headers = tokenHeader).flatMap { _ =>
+          spinCheck(100.millis, true)(Http.get(cmw / "meta" / "services", List("op" -> "stream")))(!_.payload.contains("TempRoute"))
+        }
+      }.flatMap { _ =>
+        refreshCache()
+      }.flatMap { _ =>
+        Http.get(virtualPath).map(_.status should be(404))
       }
     }
   }
