2383da8c01cce13b3eb978cf7aeeed1d31909960
Moria Yechiel <m.yechiel@refinitiv.com>
2020-01-07 11:40:16 +0200

    post merge fixes

--- server/cmwell-cons/app/extract-java-scala.sh
+++ server/cmwell-cons/app/extract-java-scala.sh
@@ -2,9 +2,9 @@
 cd $(dirname -- "$0")
 bash -c "
 cd components-extras;
-tar -xf scala-2.12.8.tgz;
+tar -xf scala-2.13.1.tgz;
 tar -xf OpenJDK8U-jdk_x64_linux_hotspot_8u232b09.tar.gz;
 mv jdk8u232-b09 java;
-mv scala-2.12.8 scala;
+mv scala-2.13.1 scala;
 
 "
